21ed3cc4f070334d16de410021256d68ed958744
Lietotajs <Lietotajs@DESKTOP-LP38FDC>
2022-02-24 21:06:13 +0200

    deleted lines

--- SuperHero/Program.cs
+++ SuperHero/Program.cs
@@ -3,7 +3,7 @@ Console.WriteLine("Hello, World!");
 
 string name = "Bubble";
 int age = 23;
-string heropower1, heropower2, heropower3;
+string heropower1; heropower2; heropower3;
 heropower1= "can fly";
 heropower2 = "shoot the bubbles";
 heropower3 = "superhuman strenght";
@@ -15,4 +15,3 @@ Console.WriteLine("****************************");
 Console.WriteLine($" Name: {name}");
 Console.WriteLine($" Age: { age}");
 Console.WriteLine($" Hero power: heropower1; heropower2; heropower3; ");
-
