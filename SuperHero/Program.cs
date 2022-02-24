b69ad62d118cd9d447d4b97853656d6f0f8160d6
Lietotajs <Lietotajs@DESKTOP-LP38FDC>
2022-02-24 20:48:47 +0200

    added hero variables

--- SuperHero/Program.cs
+++ SuperHero/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+string name = "Bubble";
+int age = 23;
+string heropower1, heropower2, heropower3;
+heropower1= "can fly";
+heropower2 = "shoot the bubbles";
+heropower3 = "superhuman strenght";
+
+
+Console.WriteLine("****************************");
+Console.WriteLine("           Hero           ")
+Console.WriteLine("****************************");
+Console.WriteLine($" Name: {name}");
+Console.WriteLine($" Age: { age}");
+Console.WriteLine($" Hero power: heropower1; heropower2; heropower3; ");
+
